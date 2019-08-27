c45655278c80448a6e2767f7976db7b5ebd0d686
chrisna2 <chrisna2@hanmail.net>
2019-08-28 08:59:54 +0900

    자바 리펙토링 과정  -> 스프링 리펙토링으로 전환하여 공부 시작

---
+++ src/main/java/com/refactoring/study/JavaRefactoringStudyApplication.java
@@ -0,0 +1,18 @@
+package com.refactoring.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRefactoringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRefactoringStudyApplication.class, args);
+	}
+	
+	
+	 
+	
+	
+	
+}
