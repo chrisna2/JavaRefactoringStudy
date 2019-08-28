e8993f62e94d6d32c5c6bd0e8cd70dd223483630
chrisna2 <chrisna2@hanmail.net>
2019-08-28 13:33:42 +0900

    2. 임시변수를 메서드 호출로 전환

---
+++ src/main/java/com/refactoring/study/arrangemethod_2/Price.java
@@ -0,0 +1,57 @@
+package com.refactoring.study.arrangemethod_2;
+
+/**
+ * 임시변수를 메서드 호출로 전환
+ * 
+ * 수행 가이드
+ * - getPirce() 메서드 내의 임시변수를 basePrice 부분을 basePrice() 메서드로 변환
+ * - getPricet() 메서드 내의 나머지 코드를 discountFactor() 메서드로 변환
+ * - junit 테스트 확인
+ * 
+ * @author chris
+ *
+ */
+public class Price {
+	
+	private int _quantity;
+	private int _itemPrice;
+	
+	public Price(int quantity, int itemPrice) {
+		this._quantity = quantity;
+		this._itemPrice = itemPrice;
+	}
+	
+	/*
+	// AS-IS
+	public double getPrice() {
+		// #1
+		int basePrice = _quantity * _itemPrice;
+		
+		// #2
+		double discountFactor;
+		if(basePrice > 1000) discountFactor = 0.95;
+		else discountFactor = 0.98;
+		return basePrice * discountFactor;
+	}	
+	*/
+	
+	// TO-BE
+	public double getPrice() {
+		return discountFactor(basePrice());
+	}
+	
+	// #2
+	private double discountFactor(int basePrice) {
+		double discountFactor;
+		if(basePrice > 1000) discountFactor = 0.95;
+		else discountFactor = 0.98;
+		
+		return basePrice * discountFactor;
+	}
+	
+	// #1
+	private int basePrice() {
+		return _quantity * _itemPrice;
+	}	
+}
+
---
+++ src/test/java/com/refactoring/study/arrangemethod_2/PriceTest.java
@@ -0,0 +1,26 @@
+package com.refactoring.study.arrangemethod_2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PriceTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetPrice_Upcase() {
+		Price price = new Price(10, 500);
+		assertTrue(price.getPrice() == 5000*0.95);
+	}
+	
+	@Test
+	public void testGetPrice_Downcase() {
+		Price price = new Price(10, 50);
+		assertTrue(price.getPrice() == 500*0.98);
+	}
+
+}
