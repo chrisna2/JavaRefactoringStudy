f8a3f5bee89b7289119d93376b8deec4df7b6d1c
chrisna2 <chrisna2@hanmail,net>
2019-10-22 16:18:52 +0900

    특정조건 확인 표시

---
+++ src/main/java/com/refactoring/study/condition_6/Employee.java
@@ -0,0 +1,21 @@
+package com.refactoring.study.condition_6;
+
+public class Employee {
+	//as-is
+	private static final double NULL_EXPENSE = -1.0;
+	private double _expenseLimit = NULL_EXPENSE;
+	private Project _primaryProject;
+	double getExpenseLimit() { 
+		_primaryProject=new Project(); 
+		
+		assert(_expenseLimit != NULL_EXPENSE || _primaryProject != null);
+		
+	    // 비용 한도를 두든지, 주요 프로젝트를 정해야한다.
+		return (_expenseLimit != NULL_EXPENSE) ?
+	       _expenseLimit:
+	       _primaryProject.getMemberExpenseLimit();
+	}
+	boolean withinLimit (double expenseAmount) {
+	   return (expenseAmount <= getExpenseLimit());
+	}
+}
---
+++ src/main/java/com/refactoring/study/condition_6/Project.java
@@ -0,0 +1,7 @@
+package com.refactoring.study.condition_6;
+
+public class Project {
+	public double getMemberExpenseLimit(){ 
+		return 100; 
+	}
+}
