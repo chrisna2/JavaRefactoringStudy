7edac8cdabfb079769be9a2c9df039d236775860
chrisna2 <chrisna2@hanmail,net>
2019-10-23 17:33:21 +0900

    sdsd

--- src/main/java/com/refactoring/study/classmove_3/Person.java
+++ src/main/java/com/refactoring/study/classmove_3/Person.java
@@ -4,6 +4,7 @@ package com.refactoring.study.classmove_3;
  * 객체구조화 3 : 클래스 추출
  * 만약 상속으로 처리할 경우 공통으로 묶는 것을 기본으로 한다. 지금 과 같은 케이스는 조금 결이 다른 경우..
  */
+
 public class Person {
 	
 	//TO-BE 너무 방대해진 클래스를 줄이기 위한 기법
