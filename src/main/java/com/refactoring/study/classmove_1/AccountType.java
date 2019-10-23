b190dabd5ad4ce7f146740c0d8fff9c3681a0dc3
chrisna2 <chrisna2@hanmail,net>
2019-10-23 13:52:01 +0900

    메서드 이동

---
+++ src/main/java/com/refactoring/study/classmove_1/AccountType.java
@@ -0,0 +1,20 @@
+package com.refactoring.study.classmove_1;
+
+public class AccountType {
+
+	public boolean isPremium() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+	
+	public double overdraftCharge(int daysOverdrawn) {
+		if (isPremium()) {
+			double result = 10;
+			if(daysOverdrawn > 7) result += (daysOverdrawn - 7) *0.85;
+				return result;			
+		}
+		else return daysOverdrawn * 1.75;
+	}
+
+}
