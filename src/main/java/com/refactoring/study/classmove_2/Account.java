79c9d53e39188e93cac3cd27b89a2924dd02ff0c
chrisna2 <chrisna2@hanmail,net>
2019-10-23 16:24:28 +0900

    2019-10-23

---
+++ src/main/java/com/refactoring/study/classmove_2/Account.java
@@ -0,0 +1,35 @@
+package com.refactoring.study.classmove_2;
+
+/**
+ * 객체구조화2 : 필드이동
+ * 클래스의 필드가 다른 필드에서 많이 사용되는 경우 해당 데이터 필드는 DTO 클래스로 전환하여 사용하는 것이 옳다.
+ * 
+ * @author 나현기
+ *
+ */
+public class Account {
+	
+	//to-be
+	AccountType type = new AccountType();
+	
+	public Account(double rate) {
+		type.set_interestRate(rate);
+	}
+	
+	public double interestForAmount_days(double amount, int days) {
+		return type.get_interestRate() * amount * days / 365;
+	}
+	
+	/* AS-IS
+	private double _interestRate = 4.53;
+	
+	public Account(double rate) {
+		this._interestRate = rate;
+	}
+	
+	public double interestForAmount_days(double amount, int days) {
+		return _interestRate * amount * days / 365;
+	}
+	 */
+
+}
---
+++ src/main/java/com/refactoring/study/classmove_2/AccountType.java
@@ -0,0 +1,18 @@
+package com.refactoring.study.classmove_2;
+
+public class AccountType {
+	
+	private double _interestRate = 4.53;
+
+	public double get_interestRate() {
+		return _interestRate;
+	}
+
+	public void set_interestRate(double _interestRate) {
+		this._interestRate = _interestRate;
+	}
+
+	public AccountType() {
+		
+	}
+}
